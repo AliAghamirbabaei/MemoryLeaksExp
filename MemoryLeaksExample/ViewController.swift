a56b3f83e83b3fcc672f1f6ea30460fd44c269ed
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-05-05 19:53:49 +0430

    Initial Commit

---
+++ MemoryLeaksExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoryLeaksExample
+//
+//  Created by Ali Aghamirbabaei on 5/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
